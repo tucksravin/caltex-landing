081a7e4393491ca1670b6cd2437ed3029ffa0a9f
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2025-01-13 10:54:39 -0800

    desktop ready for content

--- src/routes/[[preview=preview]]/+page.svelte
+++ src/routes/[[preview=preview]]/+page.svelte
@@ -32,20 +32,30 @@
   </nav>
   {#if viewpoortWidth>768}
 	  <ScreenWidthImage field={content.background_image} class="flex justify-between">
-		<div class="w-2/3 flex flex-col justify-end items-start gap-12 pb-24">
+		<div class="w-2/3 flex flex-col justify-end items-start gap-12 pb-44">
 			<img src={life} alt="life saving" class="h-20 xl:h-[165px]"/>
 			<img src={devices} alt="devices" class="h-20 xl:h-[165px]"/>
 			<img src={subtitle} alt="Protect Your Employees. Protect Your customers." class="mt-4"/>
 
 		</div>
 		<div class="flex flex-col justify-end items-end w-[30%] overflow-hidden">
-			<div class="w-full flex flex-col items-center">
+			<div class="w-full flex flex-col items-center ">
 				<PrismicImage field={content.aed} class="w-full -mb-36 z-10"/>
-				<div class="bg-[#EA7724] text px-5 pt-48  pb-12 w-1/2">
+				<div class="bg-[#EA7724] text px-5 pt-48  pb-48 w-1/2">
 					AEDs and Program Management Currently Serving the Texas Hill Country and San Antonio area.
 				</div>
 			</div>
 		</div>
 	  </ScreenWidthImage>
+	  <footer class="h-32 w-screen bg-black -mt-32 z-20 relative">
+		<ContentWidth class="h-full flex flex-col justify-center items-start gap-2">
+			<div class="text-white">
+				For information on an AED Program for Your Business or Organization:
+			</div>
+			<div class="text-white">
+				Ryan Kohnen  |  210.273.7767  |  ryan@ryankohnen.com
+			</div>
+		</ContentWidth>
+	  </footer>
   {:else}
   {/if}
